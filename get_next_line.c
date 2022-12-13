99c119f8f6f887b14e8dee1ba97fee8c5a53a039
Nazar Al Jendli <naal-jen@c1r4p8.42firenze.it>
2022-12-13 18:08:19 +0100

    heello

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: naal-jen <naal-jen@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 10:18:46 by naal-jen          #+#    #+#             */
-/*   Updated: 2022/09/13 15:42:26 by naal-jen         ###   ########.fr       */
+/*   Updated: 2022/12/13 17:38:02 by naal-jen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*delete(char *str)
+static char	*delete(char *str)
 {
 	int		i;
 	int		j;
@@ -98,8 +98,130 @@ char	*get_next_line(int fd)
 		return (NULL);
 	str = process_str(fd, buff, str);
 	if (!str)
+	{
+		free(str);
 		return (NULL);
+	}
 	new_str = process_line(str);
 	str = delete(str);
 	return (new_str);
 }
+
+
+int	main(void)
+{
+	int		fd;
+	char	*loco;
+
+	fd = 0;
+	fd = open("read_error.txt", O_RDONLY);
+	
+	loco = get_next_line(fd);
+	printf("%s", loco);
+	free(loco);
+	
+	loco = get_next_line(fd);
+	printf("%s", loco);
+	free(loco);
+	
+	loco = get_next_line(fd);
+	printf("%s", loco);
+	free(loco);
+	
+	loco = get_next_line(fd);
+	printf("%s", loco);
+	free(loco);
+
+	loco = get_next_line(fd);
+	printf("%s", loco);
+	free(loco);
+	
+	return (0);
+}
+
+
+
+
+// static char	*delete(char *str)
+// {
+// 	int		i;
+// 	int		j;
+// 	char	*new_str;
+
+// 	i = 0;
+// 	while (str[i] && str[i] != '\n')
+// 		i++;
+// 	if (!str[i])
+// 	{
+// 		free(str);
+// 		return (NULL);
+// 	}
+// 	new_str = (char *)malloc((ft_strlen(str) - i + 1) * sizeof(*new_str));
+// 	if (!new_str)
+// 		return (NULL);
+// 	i++;
+// 	j = 0;
+// 	while (str[i])
+// 		new_str[j++] = str[i++];
+// 	new_str[j] = '\0';
+// 	free(str);
+// 	return (new_str);
+// }
+
+// char	*process_line(char *str)
+// {
+// 	int		i;
+// 	char	*new_str;
+
+// 	i = 0;
+// 	if (!str[i])
+// 		return (NULL);
+// 	while (str[i] != '\n' && str[i])
+// 		i++;
+// 	new_str = (char *)malloc((i + 2) * sizeof(*new_str));
+// 	if (!new_str)
+// 		return (NULL);
+// 	i = -1;
+// 	while (str[++i] != '\n' && str[i])
+// 	{
+// 		new_str[i] = str[i];
+// 	}
+// 	if (str[i] == '\n')
+// 	{
+// 		new_str[i] = '\n';
+// 		i++;
+// 	}
+// 	new_str[i] = '\0';
+// 	return (new_str);
+// }
+
+
+// char	*get_next_line(int fd)
+// {
+// 	char		*buff;
+// 	char		*new_str;
+// 	static char	*str;
+// 	int			num;
+
+// 	num = 1;
+// 	if (fd < 0 || BUFFER_SIZE <= 0)
+// 		return (NULL);
+// 	buff = (char *)malloc((BUFFER_SIZE + 1) * sizeof(*buff));
+// 	if (!buff)
+// 		return (NULL);
+// 	while (!ft_strchr(str, '\n') && num != 0)
+// 	{
+// 		num = read(fd, buff, BUFFER_SIZE);
+// 		if (num == -1)
+// 		{
+// 			free (buff);
+// 			return (NULL);
+// 		}
+// 		buff[num] = '\0';
+// 		str = ft_strjoin(str, buff);
+// 	}
+// 	new_str = process_line(str);
+// 	str = delete(str);
+// 	free (buff);
+// 	return (new_str);
+// }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: naal-jen <naal-jen@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 13:00:19 by naal-jen          #+#    #+#             */
-/*   Updated: 2022/09/13 15:39:58 by naal-jen         ###   ########.fr       */
+/*   Updated: 2022/12/13 16:30:13 by naal-jen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,12 @@
 # include   <unistd.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1000
+#  define BUFFER_SIZE 10
 # endif
 
 char	*get_next_line(int fd);
 char	*process_str(int fd, char *buff, char *str);
 char	*process_line(char *str);
-char	*delete(char *str);
 
 char	*ft_strchr(char *s, int c);
 char	*ft_strjoin(char *str, char *buff);
