6aa400491a160393289709e81e64d3fc17683415
nazar <nazar.aljendli@digitalvirgo.com>
2025-05-11 16:08:29 +0200

    test

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,11 +6,14 @@
 /*   By: naal-jen <naal-jen@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 10:18:46 by naal-jen          #+#    #+#             */
-/*   Updated: 2022/12/14 17:00:22 by naal-jen         ###   ########.fr       */
+/*   Updated: 2025/05/11 16:08:19 by naal-jen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
 
 static char	*delete(char *str)
 {
@@ -113,3 +116,82 @@ char	*get_next_line(int fd)
 	str[fd] = delete(str[fd]);
 	return (new_str);
 }
+
+/*int	main( void)
+{
+	int		fd1;
+	int		fd2;
+	char	*line;
+
+	fd1 = open("test1.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd1 < 0)
+	{
+		printf("Error creating test1.txt\n");
+		return (1);
+	}
+	write(fd1, "This is line 1 from file 1\n", 27);
+	write(fd1, "This is line 2 from file 1\n", 27);
+	write(fd1, "This is line 3 from file 1\n", 27);
+	close(fd1);
+
+	fd2 = open("test2.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd2 < 0)
+	{
+		printf("Error creating test2.txt\n");
+		return (1);
+	}
+	write(fd2, "This is line 1 from file 2\n", 27);
+	write(fd2, "This is line 2 from file 2\n", 27);
+	close(fd2);
+	
+	fd1 = open("test1.txt", O_RDONLY);
+	fd2 = open("test2.txt", O_RDONLY);
+	
+	if (fd1 < 0 || fd2 < 0)
+	{
+		printf("Error opening test files\n");
+		return (1);
+	}
+
+	line = get_next_line(fd1);
+	printf("File 1, Line 1: %s", line);
+	free(line);
+
+	line = get_next_line(fd2);
+	printf("File 2, Line 1: %s", line);
+	free(line);
+
+	line = get_next_line(fd1);
+	printf("File 1, Line 2: %s", line);
+	free(line);
+
+	line = get_next_line(fd2);
+	printf("File 2, Line 2: %s", line);
+	free(line);
+
+	line = get_next_line(fd1);
+	printf("File 1, Line 3: %s", line);
+	free(line);
+
+	line = get_next_line(fd2);
+	if (line)
+	{
+		printf("File 2, Line 3: %s", line);
+		free(line);
+	}
+	else
+		printf("File 2, Line 3: (NULL - End of file)\n");
+	
+	line = get_next_line(-1);
+	if (line == NULL)
+		printf("Invalid FD test: Correctly returned NULL\n");
+	else
+	{
+		printf("Invalid FD test: Error - did not return NULL\n");
+		free(line);
+	}
+
+	close(fd1);
+	close(fd2);
+	return (0);
+} */
